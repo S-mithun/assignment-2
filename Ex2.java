510897ce223a67f597a1adc12cfed6e71c35ba5f
S-mithun <90100784+S-mithun@users.noreply.github.com>
2021-09-11 19:37:31 +0530

    Add files via upload

---
+++ Ex2.java
@@ -0,0 +1,11 @@
+public class Ex2 {
+     public static void main(String[] args)
+  {
+           int i=22;
+           long l=i;
+           float f=l;
+           System.out.println(" Integer value :" + i);
+           System.out.println(" Long value after converting :" + l);
+           System.out.println(" Float value after converting :" + f);
+           }
+  }
